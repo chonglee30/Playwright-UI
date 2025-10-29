e9cefcfa678e2614ddff68d6ce2309c0a131e8a0
Chong S. Lee <chonglee30@gmail.com>
2025-10-29 14:45:51 -0700

    feat: added additional reporter: github

--- playwright.config.ts
+++ playwright.config.ts
@@ -27,8 +27,7 @@ export default defineConfig<TestOptions>({
     ['html', { 
       open: 'never' //  The command will exit after tests complete. HTML report will not automatically open in your browser, and the server will not be kept alive
     }],
-    // You can also have other reporters here, e.g., 'list', 'dot', 'json'
-    // 'list', 
+    ['github'],
   ],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
